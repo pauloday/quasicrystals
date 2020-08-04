e74afef39e1508ad813b8540435d98e2cbf7accb
ertdfgcb <odaypaul@gmail.com>
2020-08-04 00:45:32 -0700

    add smooth scaling - clojure doesn't have this

--- rust/quasicrystals/src/main.rs
+++ rust/quasicrystals/src/main.rs
@@ -2,7 +2,7 @@ extern crate image;
 
 use image::{Rgb, RgbImage};
 
-fn wave(rot: f64, phase: f64, x: u32, y: u32) -> f64 {
+fn wave(rot: f64, phase: f64, x: f64, y: f64) -> f64 {
   let (srot, crot) = rot.sin_cos();
   let inner = (crot * x as f64) + (srot * y as f64) + phase;
   return (inner.cos() + 1.0) / 2.0;
@@ -24,12 +24,8 @@ fn angles(n: i64) -> Vec<f64> {
   return partitions.into_iter().map(part_to_angle).collect();
 }
 
-fn place_pixels(img: &mut RgbImage, pixel: Rgb<u8>, scale: u32, x: u32, y: u32) {
-  for x_offset in 0..scale {
-    for y_offset in 0..scale {
-      img.put_pixel(x + x_offset, y + y_offset, pixel);
-    }
-  }
+fn scaled_point(scale: u32, size: u32, point: u32) -> f64 {
+  return scale as f64 * ((2.0 * point as f64 / (size as f64 - 1.0)) - 1.0);
 }
 
 fn crystal<F: Fn(f64) -> Rgb<u8>>(
@@ -41,17 +37,17 @@ fn crystal<F: Fn(f64) -> Rgb<u8>>(
   y_max: u32) -> RgbImage {
     let angs = angles(order);
     let mut imgbuf = RgbImage::new(x_max, y_max);
-    for x in 0..(x_max / scale) {
-      for y in 0..(y_max / scale) {
-        let part_wave = |rot: &f64| wave(*rot, phase, x, y);
-        let waves = angs.iter().map(part_wave);
-        let stacked = combine(waves.collect());
-        let clamped = (1 as f64).min(stacked.max(0 as f64));
-        let shade = clamped * 255.0;
-        let pixel = colorize(shade);
-        place_pixels(&mut imgbuf, pixel, scale, x * scale, y * scale);
-      }
-    }
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+      let scaled_x = scaled_point(scale, x_max, x);
+      let scaled_y = scaled_point(scale, y_max, y);
+      let part_wave = |rot: &f64| wave(*rot, phase, scaled_x, scaled_y);
+      let waves = angs.iter().map(part_wave);
+      let stacked = combine(waves.collect());
+      let clamped = (1 as f64).min(stacked.max(0 as f64));
+      let shade = clamped * 255.0;
+      *pixel = colorize(shade);
+      // place_pixels(&mut imgbuf, pixel, scale, x * scale, y * scale);
+  }
   return imgbuf;
 }
 
@@ -72,8 +68,8 @@ fn saw_colorize(shade: f64, frame: i64, frames: i64) -> Rgb<u8> {
 
 fn main() {
   let frames = 10;
-  let colorize = |s| saw_colorize(s, 1, frames);
+  let colorize = |s| saw_colorize(s, 7, frames);
   // let colorize = |s| Rgb([s as u8, s as u8, s as u8]);
-  let frame = crystal(colorize, 1.0, 6, 3, 640, 360);
+  let frame = crystal(colorize, 1.0, 7, 32, 800, 800);
   frame.save("test.png").unwrap();
 }
